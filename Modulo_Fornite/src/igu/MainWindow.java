b0409bed9ec2af60aff1f05bb22c66d928a8227f
Luis Carlos <luisca_1994@hotmail.com>
2018-05-11 11:09:18 +0200

    11-05-2018

--- Modulo_Fornite/src/igu/MainWindow.java
+++ Modulo_Fornite/src/igu/MainWindow.java
@@ -8,11 +8,18 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import model.Catalogue;
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Toolkit;
+import java.awt.GridLayout;
 
 public class MainWindow extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JPanel pnPrincipal;
+	private JPanel pnBotones;
+	private JPanel panel;
 
 	/**
 	 * Launch the application.
@@ -35,12 +42,38 @@ public class MainWindow extends JFrame {
 	 * Create the frame.
 	 */
 	public MainWindow() {
+		setTitle("Tienda de Fortnite");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(MainWindow.class.getResource("/img/Fortnite-logo.jpg")));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 582, 382);
+		setBounds(100, 100, 851, 643);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new CardLayout(0, 0));
+		contentPane.add(getPnPrincipal(), "name_2230474924246");
 	}
 
+	private JPanel getPnPrincipal() {
+		if (pnPrincipal == null) {
+			pnPrincipal = new JPanel();
+			pnPrincipal.setLayout(new BorderLayout(0, 0));
+			pnPrincipal.add(getPnBotones(), BorderLayout.WEST);
+			pnPrincipal.add(getPanel(), BorderLayout.CENTER);
+		}
+		return pnPrincipal;
+	}
+	private JPanel getPnBotones() {
+		if (pnBotones == null) {
+			pnBotones = new JPanel();
+		}
+		return pnBotones;
+	}
+	private JPanel getPanel() {
+		if (panel == null) {
+			panel = new JPanel();
+			panel.setLayout(new GridLayout(1, 0, 0, 0));
+		}
+		return panel;
+	}
 }
